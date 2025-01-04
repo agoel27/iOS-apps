98dfc99daa3c9b902a35aa86d62dd94f3eacc046
Aryan Goel <52222311+agoel27@users.noreply.github.com>
2025-01-04 15:34:16 -0800

    Add files via upload

---
+++ I Am Poor 2/I Am Poor 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor 2
+//
+//  Created by Aryan Goel on 3/20/20.
+//  Copyright © 2020 Aryan Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
